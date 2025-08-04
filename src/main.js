3decace60ab0cf3fdf015119e8cf6388063696bd
Daniel <loureiro.daniel0@gmail.com>
2025-08-04 23:21:05 +0200

    installed GSAP

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { gsap } from 'gsap';
 
 // Scene setup
 const scene = new THREE.Scene();
